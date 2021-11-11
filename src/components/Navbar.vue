0966e3c9b050380ed0d0445e4c2675ffdf2b4b45
DanyalBani <mr.d.bani.d@gmail.com>
2021-11-11 14:27:19 +0330

    adding Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="flex px-5 py-3 justify-between">
+      <div>
+          <p>Welcome ... You're here as *</p>
+          <p>you're logged in with  ... email</p>
+      </div>
+        <router-link :to="{ name: 'Welcome' }">
+          <TheButton name="logout" :blue="true" />
+        </router-link>
+      </nav>
+</template>
+
+<script>
+import TheButton from "../components/TheButton";
+
+export default {
+  components: { TheButton },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,15 +1,16 @@
 <template>
-  <h1>fuck
-      
-  </h1>
+  <div class="bg-primary h-screen p-10 w-screen">
+    <div class="bg-white h-full rounded-xl w-full">
+      <Navbar />
+    </div>
+  </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar.vue";
 export default {
-
-}
+  components: { Navbar },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
