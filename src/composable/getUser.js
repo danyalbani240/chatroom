fbacf932a13849107008fbb6422753290fc8b8eb
DanyalBani <mr.d.bani.d@gmail.com>
2021-11-11 17:54:20 +0330

    console.log removedd

--- src/composable/getUser.js
+++ src/composable/getUser.js
@@ -3,7 +3,6 @@ import { projectAuth } from "../firebase/config";
 const user = ref(projectAuth.currentUser);
 
 projectAuth.onAuthStateChanged((_user) => {
-  console.log("state changed", _user);
   user.value = _user;
 });
 export { user };
