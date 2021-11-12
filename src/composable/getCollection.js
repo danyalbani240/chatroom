2c00d5797a7844f423ed9b27f4279f726691bed9
DanyalBani <mr.d.bani.d@gmail.com>
2021-11-12 15:32:23 +0330

    getting less snapshots from the firebase

--- src/composable/getCollection.js
+++ src/composable/getCollection.js
@@ -1,17 +1,22 @@
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { projectFirestore } from "../firebase/config";
 const getCollection = (collection) => {
   const documents = ref(null);
   let collectionRefrence = projectFirestore
     .collection(collection)
     .orderBy("created");
-  collectionRefrence.onSnapshot((snap) => {
+  const unsubscribe = collectionRefrence.onSnapshot((snap) => {
     let results = [];
     snap.docs.forEach((doc) => {
       doc.data().created && results.push({ ...doc.data(), id: doc.id });
     });
     documents.value = results;
   });
+  watchEffect((onInvalidate) => {
+    //unsubscribe when the user logged out and the collection updatet to not get more snap shots over and over again
+    onInvalidate(() => unsubscribe());
+  });
   return { documents };
 };
+
 export default getCollection;
