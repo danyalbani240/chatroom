709d10f1052ac4122fc8636991fbafd2aa59f906
DanyalBani <mr.d.bani.d@gmail.com>
2021-11-12 10:24:16 +0330

    chatField added

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -2,14 +2,16 @@
   <div class="bg-primary h-screen p-10 w-screen">
     <div class="bg-white h-full rounded-xl w-full">
       <Navbar />
+      <ChatField />
     </div>
   </div>
 </template>
 
 <script>
+import ChatField from "../components/ChatField";
 import Navbar from "../components/Navbar.vue";
 export default {
-  components: { Navbar },
+  components: { Navbar, ChatField },
 };
 </script>
 
