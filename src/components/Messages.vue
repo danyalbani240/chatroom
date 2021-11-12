f015186b0e43c6d352393b2e4807947dea38a744
DanyalBani <mr.d.bani.d@gmail.com>
2021-11-12 11:54:43 +0330

    messaged displayed in the browser

---
+++ src/components/Messages.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="overflow-y-scroll bg-Neutral-Grey6 flex-1" v-if="documents">
+    <div
+      class="mx-5 flex justify-start mt-10"
+      v-for="doc in documents"
+      :key="doc.id"
+    >
+      <div
+        class="
+          px-2
+          py-5
+          rounded-xl
+          shadow-lg
+          items-start
+          flex flex-col
+          bg-white
+        "
+        :class="{ 'bg-secondary': user.displayName == doc.name }"
+      >
+        <span class="text-Neutral-Green">{{ doc.name }} :</span>
+        <span
+
+          :class="{
+            'text-white': user.displayName == doc.name,
+            'text-primary': user.displayName != doc.name,
+          }"
+          
+          >{{ doc.message }}</span
+        >
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import getCollection from "../composable/getCollection";
+import { user } from "../composable/getUser";
+export default {
+  setup() {
+    const { documents } = getCollection("messages");
+    console.log(user.displayName);
+    return { documents, user };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="bg-primary h-screen p-10 w-screen">
-    <div class="bg-white flex flex-col justify-between h-full rounded-xl w-full">
+    <div
+      class="bg-white flex flex-col justify-between h-full rounded-xl w-full"
+    >
       <Navbar />
-      <ChatField />
+      <messages /> <ChatField />
     </div>
   </div>
 </template>
 
 <script>
 import ChatField from "../components/ChatField";
+import Messages from "../components/Messages.vue";
 import Navbar from "../components/Navbar.vue";
 export default {
-  components: { Navbar, ChatField },
+  components: { Navbar, ChatField, Messages },
 };
 </script>
 
